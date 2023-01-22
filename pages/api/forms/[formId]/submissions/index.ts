bc4d1a717375a64ede97f7588a1a004064b60383
Oliver Ni <oliver.ni@gmail.com>
2023-01-22 11:54:39 -0800

    Add email on submission

--- pages/api/forms/[formId]/submissions/index.ts
+++ pages/api/forms/[formId]/submissions/index.ts
@@ -1,10 +1,35 @@
-import { Long } from "bson";
+import sendgrid from "@sendgrid/mail";
+import { Long } from "mongodb";
 import { NextApiResponse } from "next";
 
 import { AuthMode, NextIronRequest, withSession } from "helpers/session";
 
 import { createSubmission } from "~helpers/db";
-import { User } from "~helpers/types";
+import { formium } from "~helpers/formium";
+import { Submission, User } from "~helpers/types";
+
+sendgrid.setApiKey(process.env.SENDGRID_KEY as string);
+
+const sendEmail = async (submission: Submission, formId: string) => {
+  if (!submission.email) return;
+
+  let form;
+  try {
+    form = await formium.getFormBySlug(formId);
+  } catch {
+    return;
+  }
+
+  await sendgrid.send({
+    to: submission.email,
+    from: "Pokétwo <noreply@poketwo.net>",
+    templateId: "d-2eceed634947494e93093a311b88efc9",
+    dynamicTemplateData: {
+      user: submission.user_tag,
+      form: form.name,
+    },
+  });
+};
 
 const handler = async (req: NextIronRequest, res: NextApiResponse) => {
   const { formId } = req.query;
@@ -14,13 +39,17 @@ const handler = async (req: NextIronRequest, res: NextApiResponse) => {
   const user = req.session.get<User>("user");
   if (!user) return res.status(401);
 
-  await createSubmission({
+  const _submission = {
     form_id: formId,
     user_id: Long.fromString(user.id),
     user_tag: `${user.username}#${user.discriminator}`,
     email: user.email,
     data: req.body,
-  });
+  };
+
+  const { insertedId } = await createSubmission(_submission);
+  const submission: Submission = { _id: insertedId, ..._submission };
+  await sendEmail(submission, formId);
 
   res.status(204).end();
 };
